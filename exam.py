1cf34a0ba876ee89e0558b1849344c59a3feaba1
Deepika.A <110418508+DeepikaA2004@users.noreply.github.com>
2023-07-28 19:18:25 +0530

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,8 @@
+# You don't need to add/edit anything to the below solution. 
+# Click on the SUBMIT button to solve your first problem on CodeChef.
+
+
+t=int(input())
+for i in range(t):
+    X,Y=map(int,input().split())
+    print(max(X,Y))
